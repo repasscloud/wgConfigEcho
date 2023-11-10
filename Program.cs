efc8c3477dd69882d0340bc399bc62c0a8b71e8b
Danijel-James Wynyard <danijel@repasscloud.com>
2023-11-11 00:34:25 +1100

    rebuild

--- Program.cs
+++ Program.cs
@@ -1,64 +1,98 @@
 ﻿using System.Net;
 using System.Text;
+using System.Diagnostics;
 
-namespace wgCfgEcho;
-
-class Program
+namespace wgConfigEcho
 {
-    static void Main(string[] args)
+    class Program
     {
-        // Read the PNG file into a base64 string
-        string pngFilePath = "/app/config/peer1/peer1.png";
-        string pngBase64 = FileToBase64(pngFilePath);
-
-        // Read the text file into a base64 string
-        string textFilePath = "/app/config/peer1/peer1.config";
-        string textBase64 = FileToBase64(textFilePath);
-
-        // Start listening on port 8080
-        string url = "http://localhost:8080/";
-        using (HttpListener listener = new HttpListener())
+        static void Main(string[] args)
         {
-            listener.Prefixes.Add(url);
-            listener.Start();
-            Console.WriteLine($"Listening for requests at {url}");
+            // Obtain the public IP address
+            string publicIp = GetPublicIpAddress();
+            string url = $"http://{publicIp}:8080/";
+            Console.WriteLine($"Server URL: {url}");
 
-            while (true)
+            // Read the PNG file into a base64 string
+            string pngFilePath = "/app/config/peer1/peer1.png";
+            string pngBase64 = FileToBase64(pngFilePath);
+
+            // Read the text file into a base64 string
+            string textFilePath = "/app/config/peer1/peer1.config";
+            string textBase64 = FileToBase64(textFilePath);
+
+            // Start listening on port 8080
+            using (HttpListener listener = new HttpListener())
             {
-                // Wait for a request
-                HttpListenerContext context = listener.GetContext();
-                HttpListenerRequest request = context.Request;
+                listener.Prefixes.Add(url);
+                listener.Start();
+                Console.WriteLine($"Listening for requests at {url}");
 
-                // Check if the request is a GET request
-                if (request.HttpMethod == "GET")
+                while (true)
                 {
-                    // Prepare the JSON response
-                    string jsonResponse = $"{{\"png\": \"{pngBase64}\", \"conf\": \"{textBase64}\"}}";
-                    byte[] responseBytes = Encoding.UTF8.GetBytes(jsonResponse);
+                    // Wait for a request
+                    HttpListenerContext context = listener.GetContext();
+                    HttpListenerRequest request = context.Request;
 
-                    // Send the response
-                    context.Response.ContentType = "application/json";
-                    context.Response.ContentEncoding = Encoding.UTF8;
-                    context.Response.OutputStream.Write(responseBytes, 0, responseBytes.Length);
-                    context.Response.Close();
+                    // Check if the request is a GET request
+                    if (request.HttpMethod == "GET")
+                    {
+                        // Prepare the JSON response
+                        string jsonResponse = $"{{\"png\": \"{pngBase64}\", \"conf\": \"{textBase64}\"}}";
+                        byte[] responseBytes = Encoding.UTF8.GetBytes(jsonResponse);
+
+                        // Send the response
+                        context.Response.ContentType = "application/json";
+                        context.Response.ContentEncoding = Encoding.UTF8;
+                        context.Response.OutputStream.Write(responseBytes, 0, responseBytes.Length);
+                        context.Response.Close();
+                    }
                 }
             }
         }
-    }
 
-    static string FileToBase64(string filePath)
-    {
-        try
+        static string FileToBase64(string filePath)
         {
-            // Read the file content and convert it to base64
-            byte[] fileBytes = File.ReadAllBytes(filePath);
-            return Convert.ToBase64String(fileBytes);
+            try
+            {
+                // Read the file content and convert it to base64
+                byte[] fileBytes = File.ReadAllBytes(filePath);
+                return Convert.ToBase64String(fileBytes);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading file: {ex.Message}");
+                return string.Empty;
+            }
         }
-        catch (Exception ex)
+
+        static string GetPublicIpAddress()
         {
-            Console.WriteLine($"Error reading file: {ex.Message}");
-            return string.Empty;
+            Process process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "sh",
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+
+            process.Start();
+
+            // Run a command to get the public IP address
+            process.StandardInput.WriteLine("curl -s ifconfig.me");
+
+            // Read the output of the command
+            string output = process.StandardOutput.ReadToEnd();
+
+            process.WaitForExit();
+
+            // Trim any leading or trailing whitespaces
+            return output.Trim();
         }
     }
 }
-
